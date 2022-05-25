40e407886bd3ea64aa9ea0575045ae60b9bcae2c
jjlee90 <jlee190@hotmail.com>
2022-05-25 18:32:59 -0400

    sending index and show data

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require('express')
+
+
+// Configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+console.log(PORT)
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// Listen
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT)
+})
+
+  
+
+  
