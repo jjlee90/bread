85f50506db336c38dd3a1893661bde6750828780
jjlee90 <jlee190@hotmail.com>
2022-06-01 18:30:06 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ console.log(PORT)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => {
@@ -22,6 +23,11 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 // Listen
 app.listen(PORT, () => {
     console.log('nomming at port', PORT)
