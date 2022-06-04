f6dbbee0636ecae06babe2917f051ac0a20227f9
jjlee90 <jlee190@hotmail.com>
2022-06-04 10:02:34 -0400

    delete route complete

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 // Dependencies
-const express = require('express')
-
+const express = require("express");
+// DEPENDENCIES
+const methodOverride = require("method-override");
 
 // Configuration
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-console.log(PORT)
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+console.log(PORT);
 
 // Middleware
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+// MIDDLEWARE
+app.use(methodOverride("_method"));
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to an Awesome App about Breads!");
+});
 
 // Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
 
 // 404 Page
-app.get('*', (req, res) => {
-    res.send('404')
-  })
-  
+app.get("*", (req, res) => {
+  res.send("404");
+});
+
 // Listen
 app.listen(PORT, () => {
-    console.log('nomming at port', PORT)
-})
-
-  
-
-  
+  console.log("nomming at port", PORT);
+});
